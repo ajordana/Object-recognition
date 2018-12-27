e5e4af157ab60dc6e9f88fae6516a394cc0ddd98
ajordana <jordana.armand@gmail.com>
2018-12-27 20:49:56 +0100

    data aug 2

--- data.py
+++ data.py
@@ -1,6 +1,16 @@
 import torchvision.transforms as transforms
 
 
+data_transforms_aug = transforms.Compose([
+    transforms.Resize((224, 224)),
+    transforms.RandomHorizontalFlip(),
+    transforms.ColorJitter(),
+    transforms.RandomAffine(30, translate=None, scale=None, shear=None, resample=False, fillcolor=0),
+    transforms.ToTensor(),
+    transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                 std=[0.229, 0.224, 0.225])
+])
+
 data_transforms = transforms.Compose([
     transforms.Resize((224, 224)),
     transforms.ToTensor(),
